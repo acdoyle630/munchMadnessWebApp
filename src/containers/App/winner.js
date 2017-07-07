6187861970220dbc8cb6b648e02be5aac1021992
Adam <acdoyle630@gmail.com>
2017-07-07 10:58:02 -1000

    winner details displayed

--- src/containers/App/winner.js
+++ src/containers/App/winner.js
@@ -12,11 +12,36 @@ class Winner extends Component {
 
 
   render(){
+    if(this.props.champ.champWinner.name === undefined){
+      return(
+        <Redirect to={{
+          pathname : '/'
+        }} />
+        )
+    }
+    console.log(this.props.champ.champWinner)
       return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Your Champion</h2>
+        </div>
+        <div id = "winnerDetails">
+          <div className = 'contenderName'>
+            {this.props.champ.champWinner.name}
+          </div>
+          <div className = 'contenderPrice'>
+            {this.props.champ.champWinner.price}
+          </div>
+          <div className = 'contenderStars'>
+            {this.props.champ.champWinner.rating}
+          </div>
+          <div className = 'contenderPhone'>
+            {this.props.champ.champWinner.display_phone}
+          </div>
+          <div className = 'contenderUrl'>
+            {this.props.champ.champWinner.url}
+          </div>
         </div>
       </div>
     )}
